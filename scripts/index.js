390eeefe583d0c9f2d38943ce3d8441f69e0e1f5
Christa Morera <morerachrista@gmail.com>
2025-11-24 15:00:31 -0500

    Spots Stage 5 review

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,45 @@
 const editProfilebtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 const newPostbtn = document.querySelector(".profile__new-post-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostForm = newPostModal.querySelector(".modal__form");
+const newPostImageLinkInput = newPostModal.querySelector("#image-link-input");
+const newPostImageCaptionInput = newPostModal.querySelector(
+  "#image-caption-input"
+);
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
 
 editProfilebtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
+
+//
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 
 newPostbtn.addEventListener("click", function () {
   newPostModal.classList.add("modal_is-opened");
@@ -21,3 +48,12 @@ newPostbtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleNewPostSubmit(evt) {
+  evt.preventDefault();
+  console.log(`${newPostImageLinkInput.value}`);
+  console.log(`${newPostImageCaptionInput.value}`);
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+newPostModal.addEventListener("submit", handleNewPostSubmit);
